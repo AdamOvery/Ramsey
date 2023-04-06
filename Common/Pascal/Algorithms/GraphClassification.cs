fb441dea353453c63170d5e1629259868fa1faa5
Pascal <pascal@ganaye.com>
2023-04-06 09:52:21 +0200

    I might be into something here.

--- Common/Pascal/Algorithms/GraphClassification.cs
+++ Common/Pascal/Algorithms/GraphClassification.cs
@@ -1,119 +1,81 @@
-using static Pascal.SubGraph;
+using System.Text.RegularExpressions;
 
 namespace Pascal;
 
 
 public class GraphClassification
 {
+    static Regex nodeIdRegex = new Regex(@"#\d+:", RegexOptions.Multiline);
+
+    public static string getNodeSignature(string? signatureWithNodeIds)
+    {
+        return nodeIdRegex.Replace(signatureWithNodeIds ?? "", "");
+    }
+
+    class NodeSignatureComparer : IComparer<string>
+    {
+        public int Compare(string? x, string? y)
+        {
+            return getNodeSignature(x).CompareTo(getNodeSignature(y));
+        }
+
+        public static NodeSignatureComparer instance = new NodeSignatureComparer();
+    }
+
+    private static string GetSignatureWithNodeIds(IGraph g, INode n1)
+    {
+
+        var getName = new Func<INode, int, String>((start, depth) => { return ""; });
+
+        int[] visited = new int[g.order];
+        getName = (INode start, int depth) =>
+        {
+            if (visited[start.id] > 0) return "#" + start.id + ":" + (visited[start.id] - depth);
+            else
+            {
+                visited[start.id] = depth;
+                var adjacentNodeStrings = start.adjacentNodes.Select(n =>
+                {
+                    if (n == n1) return "#" + n.id + ":0";
+                    else return getName(n, depth + 1);
+                }).ToArray();
+                Array.Sort(adjacentNodeStrings, NodeSignatureComparer.instance);
+                var result = String.Join(',', adjacentNodeStrings);
+                visited[start.id] = 0;
+                return "#" + start.id + ":[" + result + "]";
+            }
+        };
+        // getName(n1, 1)
+        var fullName = getName(n1, 1);
+        return fullName;
+    }
+
     public static void Tests()
     {
 
-
         // https://ramsey-paganaye.vercel.app/pascal/1?g6=K~{???A????S
-        var g = (G6.parse("Fg???").AsSubGraph(ClassifiedNodeGraph.factory) as ClassifiedNodeGraph)!;
-
-        var nodes = g!.nodes.Select(n => (NamedNode)n).ToList();
-        var order = nodes.Count;
+        // var g = (G6.parse("Fg???").AsSubGraph())!;
+        var g = (G6.parse("K~{???A????S").AsSubGraph())!;
 
 
-        for (var pass = 0; pass < 4; pass++)
+        foreach (var n1 in g.nodes)
         {
-            foreach (var n1 in nodes)
-            {
-                if (pass == 0)
-                {
-                    n1.name.Add("{n:" + n1.id + ", a:" + n1.adjacentNodes.Count.ToString() + "}");
-                }
-                else if (pass == 1)
-                {
-                    n1.name.Add("{n:" + n1.id + ", a:[" +
-                     String.Join(", ", n1.adjacentNodes.Select(n2 =>
-                    {
-                        var nn = (NamedNode)n2;
-                        var previousName = nn.name[pass - 1];
-                        //if (previousName.StartsWith("#" + n1.id + "(")) return "S";
-                        return previousName ?? "?";
-                    }))
-                     + "]");
-                }
-                else if (pass == 2)
-                {
-                    n1.name.Add("{n:" + n1.id + ", a:[" +
-                     String.Join(", ", n1.adjacentNodes.Select(n2 =>
-                    {
-                        var nn = (NamedNode)n2;
-                        var previousName = nn.name[pass - 1];
-                        //if (previousName.StartsWith("#" + n1.id + "(")) return "S";
-                        return previousName ?? "?";
-                    }))
-                     + "]");
-                }
-                else
-                {
-                    var nextName = n1.name[0] + "[" + String.Join(",", n1.adjacentNodes.Select(n2 =>
-                    {
-                        var nn = (NamedNode)n2;
-                        var previousName = nn.name[pass - 1];
-                        //if (previousName.StartsWith("#" + n1.id + "(")) return "S";
-                        return previousName ?? "?";
-                    })) + "]";
-                    n1.name.Add(nextName);
-                }
-            }
-            //     {
-            //         g.nodes
-
-            //         //var n = g.nodes[i] as NamedNode;
-            //         //n.name.Add(new List<int>());
-            //         //n.name[pass-1].Add(i);
-            //     }
-            //     if (pass == 1) n
-
+            var signatureWithNodeIds = GetSignatureWithNodeIds(g.graph, n1);
+            var signature = nodeIdRegex.Replace(signatureWithNodeIds, "");
+            Console.WriteLine("#" + n1.id + " full: " + signatureWithNodeIds);
+            Console.WriteLine("#" + n1.id + " abbr: " + signature);
         }
-        foreach (var n in nodes)
-        {
-            Console.WriteLine("#" + n.id);
-            foreach (var o in n.name)
-            {
-                Console.WriteLine("   " + o);
-            }
-        }
-    }
-}
-class ClassifiedNodeGraphFactory : ISubGraphFactory
-{
-    public ISubGraph CreateSubGraph(IGraph graph)
-    {
-        return new ClassifiedNodeGraph(graph);
-    }
-}
 
-class ClassifiedNodeGraph : SubGraph
-{
-    public readonly static new ClassifiedNodeGraphFactory factory = new ClassifiedNodeGraphFactory();
+        //     {
+        //         g.nodes
 
-    public ClassifiedNodeGraph(IGraph graph, IEnumerable<INode>? nodes = null) : base(graph, nodes)
-    {
+        //         //var n = g.nodes[i] as NamedNode;
+        //         //n.name.Add(new List<int>());
+        //         //n.name[pass-1].Add(i);
+        //     }
+        //     if (pass == 1) n
+
     }
 
-    override protected Node CreateNode(int id)
-    {
-        return new NamedNode(this, id);
-    }
-}
-
-class NamedNode : Node
-{
-    public List<string> name;
-
-    public NamedNode(SubGraph subGraph, int id) : base(subGraph, id)
-    {
-        name = new List<string>();
-    }
-
-    override public string ToString()
-    {
-        return string.Join(" ", name);
-    }
 }
 
