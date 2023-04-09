b6e7a3eb7e641e4fcef220912b3c4dc47e4bdbe9
Pascal <pascal@ganaye.com>
2023-04-09 12:49:54 +0200

    New menu

--- Common/Program.cs
+++ Common/Program.cs
@@ -16,5 +16,44 @@ internal class Program
         {
             AdamProgram.AdamMain();
         }
+
+        Menu();
+    }
+
+    private static void Menu()
+    {
+        while (true)
+        {
+            Console.WriteLine(@"______                               ");
+            Console.WriteLine(@"| ___ \                              ");
+            Console.WriteLine(@"| |_/ /__ _ _ __ ___  ___  ___ _   _ ");
+            Console.WriteLine(@"|    // _` | '_ ` _ \/ __|/ _ \ | | |");
+            Console.WriteLine(@"| |\ \ (_| | | | | | \__ \  __/ |_| |");
+            Console.WriteLine(@"\_| \_\__,_|_| |_| |_|___/\___|\__, |");
+            Console.WriteLine(@"                                __/ |");
+            Console.WriteLine(@"                               |___/ ");
+            Console.WriteLine(@"[A]   Adam");
+            Console.WriteLine(@"[P]   Pascal");
+            Console.WriteLine(@"[Esc] Exit");
+            Console.Write("> ");
+            var input = Console.ReadKey().KeyChar.ToString().ToUpper();
+            Console.WriteLine();
+
+            switch (input)
+            {
+                case "A":
+                    AdamProgram.AdamMain();
+                    break;
+                case "P":
+                    PascalProgram.PascalMain();
+                    break;
+                case "\x1B":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+            }
+        }
     }
 }
